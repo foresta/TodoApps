40d991f212056a65b13e6023738303f89f1bbc55
k_morita <tt.k.y.nuwstar.kz@gmail.com>
2015-06-28 21:43:13 +0900

    add task, toggle todo and done

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,7 @@
     var inputTag = document.getElementById('input');
     var submitBtn = document.getElementById('submit');
     var listWrap = document.getElementById('list_wrap');
+    var checkboxes = document.getElementsByClassName('check');
 
     submitBtn.addEventListener('click', function() {
         var text = inputTag.value;
@@ -13,4 +14,21 @@
         li.innerHTML = '<p><input type="checkbox" class="check" />' + text + '</p>';
         listWrap.insertBefore(li, listWrap.firstChild);
     });
+
+    for (var i = 0; i < checkboxes.length;i++){
+        checkboxes[i].addEventListener('click', onChecked, false);
+    }
+    
+    function onChecked(){
+        var self = this;
+        var parentTag = self.parentNode;
+        if (parentTag.className == "done")
+        {
+            parentTag.className = "";
+        }
+        else {
+            parentTag.className = "done";
+        }
+        return false;
+    }
 })();
